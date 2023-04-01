4b5c182ee92ab941e9140d43c8a7eef32cbb885e
Emanuel Joness <itzemanuelj@gmail.com>
2023-03-31 22:40:26 -0400

    mvp commit for Python Variables

---
+++ 5 Python Variables/Python Variables.py
@@ -0,0 +1,33 @@
+# Python Variables
+
+# Creating Variables
+name = "Justin"
+
+age = 28
+
+print(name)
+print(age)
+
+# Casting
+lname = str("Jones")
+age = int(28)
+age = float(28)
+
+print(lname)
+print(age)
+print(age)
+
+# Get the Type
+print(type(age))
+print(type(name))
+
+# Single or Double Quotes?
+name = 'Justin'
+lname = "Jones"
+
+# Case-Sensitive
+a = True
+A = False
+
+# A cannot overwrite a
+
