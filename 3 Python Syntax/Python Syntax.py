d71b421056cea180cb9b3c0b186c72a455f5c872
Emanuel Joness <itzemanuelj@gmail.com>
2023-04-10 23:40:50 -0400

    Update Python Syntax.py

--- 3 Python Syntax/Python Syntax.py
+++ 3 Python Syntax/Python Syntax.py
@@ -20,3 +20,17 @@ if 5 > 2:
         print("5 is greater than 2")
 if 5 > 2:
                 print("5 is greater than 2")
+
+# Python Variables
+
+x = 1
+y = "Hello World"
+
+# Comments
+
+# This is a comment
+# x = 1
+# y = 2
+
+
+
