34072febe3bd7cdc37148cc6cc6c7ee4a9a94726
Emanuel Joness <itzemanuelj@gmail.com>
2023-04-15 16:04:57 -0400

    Update Python Data Types.py

--- 6 Python Data Types/Python Data Types.py
+++ 6 Python Data Types/Python Data Types.py
@@ -15,4 +15,55 @@ print(type("2"))
 ## string
 
 print(type(2))
-## number
+## int
+
+x = "Hello Wolrd" ## str
+
+x = 1 ## int
+
+x = 1.0 ## float
+
+x = 1j ##comlex
+
+x = ["apple", "apple", "apple", "apple"] ## list
+
+x = ("apple", "apple", "apple", "apple") ## tuple  
+
+x = range(4) ##range 
+
+
+## Setting the Specific Data Type
+
+x = str("Hello World") ## str
+
+x = int(1) ## int
+
+x = float(1.0) ## float
+
+## x = {"name": justin, "age": 28} ## dict
+
+x = {"appple", "bannana", "cherry"}  ## set
+
+x = frozenset({"apple", "banana", "cherry"})	## frozenset
+
+x = True ##bool
+
+x = b"Hello"	##bytes
+
+x = bytearray(5)	##byterry
+
+x = memoryview(bytes(5))	##memorybite
+
+x = None ## nonetype
+
+
+
+
+
+
+
+
+
+
+
+
